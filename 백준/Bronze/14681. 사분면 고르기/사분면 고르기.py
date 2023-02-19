de62908ee09e1977636b288f570fa7b4c1bf783c
hermanodoo <68317526+hermanodoo@users.noreply.github.com>
2023-02-19 15:02:17 +0900

    [Bronze V] Title: 사분면 고르기, Time: 32 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -1,3 +1,14 @@
-x = int(input())
-y = int(input())
-print((1 if y > 0 else 4) if x > 0 else (2 if y > 0 else 3))
+a = int(input())
+b = int(input())
+
+if a > 0:
+    if b > 0:
+        print(1)
+    else:
+        print(4)
+else:
+    if b > 0:
+        print(2)
+    else:
+        print(3)
+    
