10da3197edfc1b40dd10c0a4aa3369e2ccaf05fb
hermanodoo <68317526+hermanodoo@users.noreply.github.com>
2023-03-19 23:50:00 +0900

    [Bronze V] Title: A-B, Time: 128 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int num1 = Integer.parseInt(input[0]);
+        int num2 = Integer.parseInt(input[1]);
+        System.out.print(num1 - num2);
+    }
+}
